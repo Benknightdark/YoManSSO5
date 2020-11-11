52fa959a8bd084b2f74bee1415112dee7884dafc
BenKung <moon791017@gmail.com>
2020-11-11 17:41:43 +0800

    OK

--- Services/Helpers/ConfigService.cs
+++ Services/Helpers/ConfigService.cs
@@ -1,51 +1,42 @@
-﻿using OAthLib.Models.FB;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OAthLib.Models.FB;
 using OAthLib.Models.Google;
 using OAthLib.Models.Line;
 using OAthLib.Models.LinkedIn;
 using OAthLib.Models.MS;
-using Microsoft.Extensions.Configuration;
-namespace OAthLib.Services.Helpers
-{
-    public class ConfigService
-    {
-        public  IConfiguration _config { get; }
-        public ConfigService(IConfiguration config)
-        {
-            _config = config;
 
+namespace OAthLib.Services.Helpers {
+    public class ConfigService {
+        public IConfiguration configuration { get; }
+
+        public ConfigService (IConfiguration configuration) {
+            this.configuration = configuration;
 
         }
-        public LineConfig GetLineConfig()
-        {
-            var appConfig = new LineConfig();
+        public LineConfig GetLineConfig () {
+            var appConfig = System.Text.Json.JsonSerializer.Deserialize<LineConfig> (configuration.GetSection ("Line").Value);
+            return appConfig;
+
+        }
+        public FBConfig GetFBConfig () {
+
+            var appConfig = System.Text.Json.JsonSerializer.Deserialize<FBConfig> (configuration.GetSection ("FB").Value);
+            return appConfig;
+        }
+        public GoogleConfig GetGoogleConfig () {
+            var appConfig = System.Text.Json.JsonSerializer.Deserialize<GoogleConfig> (configuration.GetSection ("Google").Value);
+            return appConfig;
             
-            _config.GetSection("Line").Bind(appConfig);
-            return appConfig;
+        }
+        public LinkedInConfig GetLinkedInConfig () {
+            
 
-
-        }
-        public FBConfig GetFBConfig()
-        {
-            var appConfig = new FBConfig();
-            _config.GetSection("FB").Bind(appConfig);
+             var appConfig = System.Text.Json.JsonSerializer.Deserialize<LinkedInConfig> (configuration.GetSection ("LinkedIn").Value);
             return appConfig;
         }
-          public GoogleConfig GetGoogleConfig()
-        {
-            var appConfig = new GoogleConfig();
-            _config.GetSection("Google").Bind(appConfig);
-            return appConfig;
-        }
-        public LinkedInConfig GetLinkedInConfig()
-        {
-            var appConfig = new LinkedInConfig();
-            _config.GetSection("LinkedIn").Bind(appConfig);
-            return appConfig;
-        }
-        public MSConfig GetMSConfig()
-        {
-            var appConfig = new MSConfig();
-            _config.GetSection("MS").Bind(appConfig);
+        public MSConfig GetMSConfig () {
+             var appConfig = System.Text.Json.JsonSerializer.Deserialize<MSConfig> (configuration.GetSection ("MS").Value);
             return appConfig;
         }
     }
